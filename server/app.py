d72adae8181c1ee154f98e15802eadc5736eddc3
= <=>
2023-10-03 23:42:39 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -20,15 +20,29 @@ def clear_session():
     session['page_views'] = 0
     return {'message': '200: Successfully cleared session data.'}, 200
 
-@app.route('/articles')
+@app.route("/articles")
 def index_articles():
+    arts = [art.to_dict() for art in Article.query.all()]
+    response = make_response(arts, 200)
+    return response
 
-    pass
 
-@app.route('/articles/<int:id>')
+@app.route("/articles/<int:id>")
 def show_article(id):
+    art = Article.query.filter(Article.id == id).first().to_dict()
 
-    pass
+    session["page_views"] = (
+        session["page_views"] + 1 if session.get("page_views") else 1
+    )
+
+    if session.get("page_views") <= 3:
+        response = make_response(art, 200)
+        return response
+
+    elif session.get("page_views") > 3:
+        message = {"message": "Maximum pageview limit reached"}
+        response = make_response(message, 401)
+        return response
 
 if __name__ == '__main__':
     app.run(port=5555)
