508a19ffce80212ac3b0dd343c72aa2759188d0a
= <=>
2023-10-04 00:03:11 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -6,19 +6,21 @@ from flask_migrate import Migrate
 from models import db, Article, User
 
 app = Flask(__name__)
-app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = b"Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/clear')
+
+@app.route("/clear")
 def clear_session():
-    session['page_views'] = 0
-    return {'message': '200: Successfully cleared session data.'}, 200
+    session["page_views"] = 0
+    return {"message": "200: Successfully cleared session data."}, 200
+
 
 @app.route("/articles")
 def index_articles():
@@ -44,5 +46,6 @@ def show_article(id):
         response = make_response(message, 401)
         return response
 
-if __name__ == '__main__':
-    app.run(port=5556)
+
+if __name__ == "__main__":
+    app.run(port=5555)
